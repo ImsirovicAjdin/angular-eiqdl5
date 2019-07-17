6fc893318489800adfc740f859d1bb18584ee608
ImsirovicAjdin <ajdika@live.com>
2019-07-17 17:06:04 +0200

    Add ngFor repeater to app.component.ts's template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    minimalism
+    <div>
+      <div *ngFor="let car of cars">
+        <div style="min-height:100px; max-width: 200px; border: 1px solid gray; color: white; background: tomato; margin-bottom: 20px; padding: 20px;">
+
+
+          <p>{{car.make}}</p>
+          <p>{{car.color}}</p>
+
+
+        </div>
+      </div>
+    </div>
   `
 })
 export class AppComponent  {
+
+  cars = [
+    {
+      id: 1,
+      make: 'Toyota',
+      color: 'red'
+    },
+    {
+      id: 2,
+      make: 'VW',
+      color: 'gray'
+    },
+    {
+      id: 3,
+      make: 'Chrysler',
+      color: 'blue'
+    }
+  ]
+
 }
